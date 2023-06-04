e601d9016206b00c89164786b6a888ee1957ef8a
BakhytzhanAmanzholov <amanzholovbakhytzhan@gmail.com>
2023-06-05 00:03:10 +0600

    fix

--- src/main/java/kz/fitness/homefit/HomeFitApplication.java
+++ src/main/java/kz/fitness/homefit/HomeFitApplication.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import kz.fitness.homefit.dto.request.TrainingDto;
 import kz.fitness.homefit.models.Account;
 import kz.fitness.homefit.models.Exercise;
+import kz.fitness.homefit.models.Training;
 import kz.fitness.homefit.services.AccountService;
 import kz.fitness.homefit.services.ExerciseService;
+import kz.fitness.homefit.services.TrainingService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +15,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
 
 @SpringBootApplication
 public class HomeFitApplication {
@@ -39,7 +44,8 @@ public class HomeFitApplication {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(ExerciseService exerciseService, AccountService accountService) {
+    public CommandLineRunner commandLineRunner(ExerciseService exerciseService, AccountService accountService,
+                                               TrainingService trainingService) {
         return args -> {
             exerciseService.save(
                     Exercise.builder()
@@ -48,7 +54,7 @@ public class HomeFitApplication {
                             .build()
             );
 
-            exerciseService.save(
+            Exercise squats = exerciseService.save(
                     Exercise.builder()
                             .name("Squats")
                             .type(Exercise.Type.LEGS)
@@ -62,17 +68,29 @@ public class HomeFitApplication {
                             .build()
             );
 
-            accountService.save(
+            Account account = accountService.save(
                     Account.builder()
                             .age(20)
                             .email("abc123@gmail.com")
                             .password("abc123")
                             .fullName("Hello")
                             .gender(Account.Gender.MALE)
-                            .count(0)
-                            .calories(0)
+                            .count(1)
+                            .calories(32)
+                            .trainings(new HashSet<>())
                             .build()
             );
+            Training training = trainingService.save(
+                    Training.builder()
+                            .account(account)
+                            .location("ml-server/video/dca954aa-695e-4d8b-9fc4-13dea4ab21f3_processed.mp4")
+                            .time(14L)
+                            .exercise(squats)
+                            .dateTime(LocalDateTime.now())
+                            .build()
+            );
+            account = accountService.findByEmail(account.getEmail());
+            accountService.addTrainingToAccount(training, account);
         };
     }
 
